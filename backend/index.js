94cfbfb408c5bb2b44aa0674969e5bdda134529f
NetsanetAlemu <netsialemu1907@gmail.com>
2023-11-21 15:17:05 +0300

    updated DB and added UI

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import mysql from "mysql"
+import cors from "cors"
 
 const app = express()
 
@@ -9,6 +10,9 @@ const db = mysql.createConnection({
     password: "root",
     database: "test"
 });
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.json("hello this is the backend")
@@ -21,6 +25,20 @@ app.get("/books", (req, res) => {
         return res.json(data)
     })
 })
+
+ app.post("/books", (req, res) => {
+    const q = "insert into books (`title`, `desc`, `price`, cover`) values (?)";
+    const values = [
+        req.body.title, 
+        req.body.desc, 
+        req.body.price,
+        req.body.cover
+    ];
+    db.query(q, [values], (err, data) => {
+        if(err) return res.json(err);
+        return res.json("Book has been created successfully.");
+    })
+ })
 
 app.listen(8800, () => {
     console.log(("Connected to backend!"))
